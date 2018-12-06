4b0b48ca112727868f7eb4ec4e19c84232407e14
T-R0D <thenriod@gmail.com>
2018-12-05 23:10:36 -0800

    Day04

--- aoc2018/day04/solution.py
+++ aoc2018/day04/solution.py
@@ -1,16 +1,109 @@
+import collections
+import datetime
+
+
 def main():
     puzzle_input = parse_input()
     print(part_one(puzzle_input))
     print(part_two(puzzle_input))
 
+
 def parse_input():
-    pass
+    logs = []
+    with open('input.txt') as f:
+        for line in f:
+            timestamp_str, activity = line.split('] ')
+            timestamp_str = timestamp_str[1:]
+            logs.append((timestamp_str, activity))
+
+    def get_timestamp(log):
+        return log[0]
+    logs = list(sorted(logs, key=get_timestamp))
+
+    return logs
+
 
 def part_one(puzzle_input):
-    pass
+    minutes_slept_by_guard = collections.Counter()
+    minutes_slept_table = {}
+    current_guard = -1
+    sleep_time = None
+    for log in puzzle_input:
+        timestamp = datetime.datetime.strptime(log[0], '%Y-%m-%d %H:%M')
+        event = log[1]
+        if 'begins shift' in event:
+            current_guard = extract_guard_id(event)
+        elif 'falls asleep' in event:
+            sleep_time = timestamp
+        else:
+            minutes_slept = (timestamp - sleep_time).total_seconds() // 60
+            minutes_slept_by_guard[current_guard] += minutes_slept
+
+            histogram = minutes_slept_table.get(current_guard, collections.Counter())
+            if sleep_time.minute >= 0:
+                for i in range(sleep_time.minute, timestamp.minute):
+                    histogram[i] += 1
+            else:
+                if timestamp.minute > sleep_time.minute:
+                    for i in range(sleep_time.minute, timestamp.minute):
+                        histogram[i] += 1
+                else:
+                    for i in range(sleep_time.minute, 0):
+                        histogram[i] += 1
+                    for i in range(0, timestamp.minute):
+                        histogram[i] += 1
+            minutes_slept_table[current_guard] = histogram
+
+    sleepiest_guard = minutes_slept_by_guard.most_common(1)[0][0]
+    histogram = minutes_slept_table[sleepiest_guard]
+    sleepiest_minute = histogram.most_common(1)[0][0]
+
+    return str(sleepiest_guard * sleepiest_minute)
 
 def part_two(puzzle_input):
-    pass
+    minutes_slept_table = {}
+    current_guard = -1
+    sleep_time = None
+    for log in puzzle_input:
+        timestamp = datetime.datetime.strptime(log[0], '%Y-%m-%d %H:%M')
+        event = log[1]
+        if 'begins shift' in event:
+            current_guard = extract_guard_id(event)
+        elif 'falls asleep' in event:
+            sleep_time = timestamp
+        else:
+            histogram = minutes_slept_table.get(current_guard, collections.Counter())
+            if sleep_time.minute >= 0:
+                for i in range(sleep_time.minute, timestamp.minute):
+                    histogram[i] += 1
+            else:
+                if timestamp.minute > sleep_time.minute:
+                    for i in range(sleep_time.minute, timestamp.minute):
+                        histogram[i] += 1
+                else:
+                    for i in range(sleep_time.minute, 0):
+                        histogram[i] += 1
+                    for i in range(0, timestamp.minute):
+                        histogram[i] += 1
+            minutes_slept_table[current_guard] = histogram
+
+    regularly_asleep_guard = -1
+    global_sleepiest_minute = -1
+    sleepy_minute_frequency = -1
+    for guard_id, histogram in minutes_slept_table.items():
+        sleepiest_minute, frequency = histogram.most_common(1)[0]
+        if frequency > sleepy_minute_frequency:
+            global_sleepiest_minute = sleepiest_minute
+            regularly_asleep_guard = guard_id
+            sleepy_minute_frequency = frequency
+
+    return str(regularly_asleep_guard * global_sleepiest_minute)
+
+
+def extract_guard_id(shift_start_str):
+    number_str = shift_start_str.split(' ')[1]
+    return int(number_str.replace('#', ''))
+
 
 if __name__ == '__main__':
     main()
--- aoc2018/day04/test/test_solution.py
+++ aoc2018/day04/test/test_solution.py
@@ -1,7 +1,7 @@
 import unittest
-import day{0:02d}.solution as solution
+import day04.solution as solution
 
-class TestDay{0:02d}(unittest.TestCase):
+class TestDay04(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -9,7 +9,47 @@ class TestDay{0:02d}(unittest.TestCase):
         pass
 
     def test_part_one(self):
-        pass
+        test_input = [
+            ('1518-11-01 00:00', 'Guard #10 begins shift',),
+            ('1518-11-01 00:05', 'falls asleep',),
+            ('1518-11-01 00:25', 'wakes up',),
+            ('1518-11-01 00:30', 'falls asleep',),
+            ('1518-11-01 00:55', 'wakes up',),
+            ('1518-11-01 23:58', 'Guard #99 begins shift',),
+            ('1518-11-02 00:40', 'falls asleep',),
+            ('1518-11-02 00:50', 'wakes up',),
+            ('1518-11-03 00:05', 'Guard #10 begins shift',),
+            ('1518-11-03 00:24', 'falls asleep',),
+            ('1518-11-03 00:29', 'wakes up',),
+            ('1518-11-04 00:02', 'Guard #99 begins shift',),
+            ('1518-11-04 00:36', 'falls asleep',),
+            ('1518-11-04 00:46', 'wakes up',),
+            ('1518-11-05 00:03', 'Guard #99 begins shift',),
+            ('1518-11-05 00:45', 'falls asleep',),
+            ('1518-11-05 00:55', 'wakes up',),
+        ]
+
+        self.assertEqual('240', solution.part_one(test_input))
 
     def test_part_two(self):
-        pass
+        test_input = [
+            ('1518-11-01 00:00', 'Guard #10 begins shift',),
+            ('1518-11-01 00:05', 'falls asleep',),
+            ('1518-11-01 00:25', 'wakes up',),
+            ('1518-11-01 00:30', 'falls asleep',),
+            ('1518-11-01 00:55', 'wakes up',),
+            ('1518-11-01 23:58', 'Guard #99 begins shift',),
+            ('1518-11-02 00:40', 'falls asleep',),
+            ('1518-11-02 00:50', 'wakes up',),
+            ('1518-11-03 00:05', 'Guard #10 begins shift',),
+            ('1518-11-03 00:24', 'falls asleep',),
+            ('1518-11-03 00:29', 'wakes up',),
+            ('1518-11-04 00:02', 'Guard #99 begins shift',),
+            ('1518-11-04 00:36', 'falls asleep',),
+            ('1518-11-04 00:46', 'wakes up',),
+            ('1518-11-05 00:03', 'Guard #99 begins shift',),
+            ('1518-11-05 00:45', 'falls asleep',),
+            ('1518-11-05 00:55', 'wakes up',),
+        ]
+
+        self.assertEqual('4455', solution.part_two(test_input))
