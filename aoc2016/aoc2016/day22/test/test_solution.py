c1eaa16fe46d21add55158348b524b2e1295a00f
Terence Henriod <__t_r0d__@TMBP.local>
2024-10-18 17:08:33 -0700

    [AOC2016] Day 22

---
+++ aoc2016/aoc2016/day22/test/test_solution.py
@@ -0,0 +1,38 @@
+# This file is part of aoc2016.
+#
+# aoc2016 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# aoc2016 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with aoc2016.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import aoc2016.day22.solution as day22
+
+TEST_GRID_DF_OUTPUT = """root@test-gridcenter# df -h
+Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%"""
+
+
+class TestSolution(unittest.TestCase):
+    def test_part_two_counts_steps_to_move_target_data_to_origin_node_correctly(self):
+        expected = "7"
+
+        result = day22.part_two(TEST_GRID_DF_OUTPUT)
+
+        self.assertEqual(result, expected)
