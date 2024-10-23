13f94a833cd3c96c035d472f2dd090781146e532
Terence Henriod <__t_r0d__@TMBP.local>
2024-10-23 08:45:10 -0700

    [AOC2018] Day 11

--- aoc2018/aoc2018/day11/solution.py
+++ aoc2018/aoc2018/day11/solution.py
@@ -1,16 +1,99 @@
-def main():
-    puzzle_input = parse_input()
-    print(part_one(puzzle_input))
-    print(part_two(puzzle_input))
+import collections
 
-def parse_input():
-    pass
 
 def part_one(puzzle_input):
-    pass
+    grid_serial_number = parse_grid_serial_number(puzzle_input)
+
+    grid_size = 300
+
+    grid = generate_power_level_grid(grid_serial_number, grid_size)
+    summed_power_table = compute_summed_power_table(grid)
+
+    best_block_coordinates = (0, 0)
+    best_block_power = -5 * (grid_size * grid_size)
+    block_size = 3
+    for i in range(len(summed_power_table) - (block_size - 1)):
+        for j in range(len(summed_power_table[0]) - (block_size - 1)):
+            block_power = compute_block_power(summed_power_table, i, j, block_size)
+
+            if block_power > best_block_power:
+                best_block_coordinates = (j + 1, i + 1)
+                best_block_power = block_power
+
+    return str(best_block_coordinates)
+
 
 def part_two(puzzle_input):
-    pass
+    grid_serial_number = parse_grid_serial_number(puzzle_input)
 
-if __name__ == '__main__':
-    main()
+    grid_size = 300
+
+    grid = generate_power_level_grid(grid_serial_number, grid_size)
+    summed_power_table = compute_summed_power_table(grid)
+
+    best_block_coordinates = (0, 0)
+    best_block_power = -5 * (grid_size * grid_size)
+    best_block_size = 0
+    for block_size in range(1, 300 + 1):
+        for i in range(len(summed_power_table) - (block_size - 1)):
+            for j in range(len(summed_power_table[0]) - (block_size - 1)):
+                block_power = compute_block_power(summed_power_table, i, j, block_size)
+
+                if block_power > best_block_power:
+                    best_block_coordinates = (j + 1, i + 1)
+                    best_block_power = block_power
+                    best_block_size = block_size
+
+    return str((best_block_coordinates, best_block_size))
+
+
+def parse_grid_serial_number(puzzle_input):
+    return int(puzzle_input)
+
+
+def generate_power_level_grid(grid_serial_number, size):
+    return [
+        [compute_power_level(grid_serial_number, x, y) for x in range(1, size + 1)]
+        for y in range(1, size + 1)
+    ]
+
+
+def compute_power_level(grid_serial_number, x, y):
+    rack_id = x + 10
+    power_level = rack_id * y
+    power_level += grid_serial_number
+    power_level *= rack_id
+    power_level = (power_level // 100) % 10
+    power_level -= 5
+    return power_level
+
+
+def compute_summed_power_table(grid):
+    table = [[grid[i][j] for j in range(len(grid[0]))] for i in range(len(grid))]
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            if i - 1 >= 0:
+                table[i][j] += table[i - 1][j]
+
+            if j - 1 >= 0:
+                table[i][j] += table[i][j - 1]
+
+            if i - 1 >= 0 and j - 1 >= 0:
+                table[i][j] -= table[i - 1][j - 1]
+
+    return table
+
+
+def compute_block_power(summed_power_table, i, j, block_size):
+    block_power = summed_power_table[i + (block_size) - 1][j + (block_size) - 1]
+
+    if i - 1 >= 0:
+        block_power -= summed_power_table[i - 1][j + (block_size - 1)]
+
+    if j - 1 >= 0:
+        block_power -= summed_power_table[i + (block_size - 1)][j - 1]
+
+    if i - 1 >= 0 and j - 1 >= 0:
+        block_power += summed_power_table[i - 1][j - 1]
+
+    return block_power
--- aoc2018/aoc2018/day11/test/test_solution.py
+++ aoc2018/aoc2018/day11/test/test_solution.py
@@ -1,15 +1,28 @@
 import unittest
-import day{0:02d}.solution as solution
+import aoc2018.day11.solution as solution
 
-class TestDay{0:02d}(unittest.TestCase):
-    def setUp(self):
-        pass
 
-    def tearDown(self):
-        pass
+class TestDay11(unittest.TestCase):
+    def test_compute_power_level_returns_correct_power_level(self):
+        test_cases = [
+            (8, 3, 5, 4),
+            (57, 122, 79, -5),
+            (39, 217, 196, 0),
+            (71, 101, 153, 4),
+        ]
+        for grid_serial_number, x, y, expected in test_cases:
+            with self.subTest(f"{grid_serial_number} {x},{y} => {expected}"):
+                result = solution.compute_power_level(grid_serial_number, x, y)
 
-    def test_part_one(self):
-        pass
+                self.assertEqual(result, expected)
 
-    def test_part_two(self):
-        pass
+    def test_find_coordinates_of_highest_powered_cell_block_works(self):
+        test_cases = [
+            ("18", "(33, 45)"),
+            ("42", "(21, 61)"),
+        ]
+        for grid_serial_number, expected in test_cases:
+            with self.subTest(f"{grid_serial_number} => {expected}"):
+                result = solution.part_one(grid_serial_number)
+
+                self.assertEqual(result, expected)
