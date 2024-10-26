72055d181a27514037a963ff3e2d2f8ce37d6f2f
Terence Henriod <__t_r0d__@TMBP.local>
2024-10-25 21:36:19 -0700

    [AOC2018] Day 14

--- aoc2018/aoc2018/day14/solution.py
+++ aoc2018/aoc2018/day14/solution.py
@@ -1,16 +1,78 @@
-def main():
-    puzzle_input = parse_input()
-    print(part_one(puzzle_input))
-    print(part_two(puzzle_input))
+import collections
 
-def parse_input():
-    pass
 
 def part_one(puzzle_input):
-    pass
+    target = parse_target_number_of_recipes(puzzle_input)
+
+    next_ten_recipe_scores = find_ten_recipe_scores_after_target_number_of_recipes(
+        int(target)
+    )
+
+    return "".join(str(score) for score in next_ten_recipe_scores)
+
 
 def part_two(puzzle_input):
-    pass
+    target = parse_target_number_of_recipes(puzzle_input)
 
-if __name__ == '__main__':
-    main()
+    n_recipes_before_target = find_n_recipes_before_target_score_sequence(target)
+
+    return str(n_recipes_before_target)
+
+
+def parse_target_number_of_recipes(puzzle_input):
+    return puzzle_input
+
+
+def find_ten_recipe_scores_after_target_number_of_recipes(target):
+    held_recipe_ids = [0, 1]
+    scoreboard = [3, 7]
+    while len(scoreboard) < target + 10:
+        new_recipe_score = sum(scoreboard[id] for id in held_recipe_ids)
+
+        if new_recipe_score <= 9:
+            scoreboard.append(new_recipe_score)
+        else:
+            scoreboard.append(new_recipe_score // 10)
+            scoreboard.append(new_recipe_score % 10)
+
+        held_recipe_ids = [
+            (id + 1 + scoreboard[id]) % len(scoreboard) for id in held_recipe_ids
+        ]
+
+    next_ten_recipe_scores = scoreboard[target : target + 10]
+    return next_ten_recipe_scores
+
+
+def find_n_recipes_before_target_score_sequence(target):
+    target_sequence = [int(x) for x in target]
+
+    held_recipe_ids = [0, 1]
+    scoreboard = [3, 7]
+
+    window = collections.deque(scoreboard)
+    n = 0
+
+    while True:
+        new_recipe_score = sum(scoreboard[id] for id in held_recipe_ids)
+
+        new_scores = []
+        if new_recipe_score <= 9:
+            new_scores.append(new_recipe_score)
+        else:
+            new_scores.append(new_recipe_score // 10)
+            new_scores.append(new_recipe_score % 10)
+
+        for new_score in new_scores:
+            scoreboard.append(new_score)
+
+            window.append(new_score)
+            if len(window) > len(target_sequence):
+                window.popleft()
+                n += 1
+
+            if list(window) == target_sequence:
+                return n
+
+        held_recipe_ids = [
+            (id + 1 + scoreboard[id]) % len(scoreboard) for id in held_recipe_ids
+        ]
--- aoc2018/aoc2018/day14/test/test_solution.py
+++ aoc2018/aoc2018/day14/test/test_solution.py
@@ -1,15 +1,31 @@
 import unittest
-import day{0:02d}.solution as solution
+import aoc2018.day14.solution as solution
 
-class TestDay{0:02d}(unittest.TestCase):
-    def setUp(self):
-        pass
+class TestDay14(unittest.TestCase):
+    def test_part_one_determines_ten_scores_after_target(self):
+        test_cases = [
+            ("9", "5158916779"),
+            ("5", "0124515891"),
+            ("18", "9251071085"),
+            ("2018", "5941429882"),
+        ]
 
-    def tearDown(self):
-        pass
+        for target, expected in test_cases:
+            with self.subTest(f"{target} -> {expected}"):
+                result = solution.part_one(target)
 
-    def test_part_one(self):
-        pass
+                self.assertEqual(result, expected)
 
-    def test_part_two(self):
-        pass
+    def test_part_two_counts_recipes_before_target_score_sequence(self):
+        test_cases = [
+            ("51589", "9"),
+            ("01245", "5"),
+            ("92510", "18"),
+            ("59414", "2018"),
+        ]
+
+        for target, expected in test_cases:
+            with self.subTest(f"{target} -> {expected}"):
+                result = solution.part_two(target)
+
+                self.assertEqual(result, expected)
