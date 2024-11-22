8c331d4711648a5caeff4026fbaa8f17a6a420a1
Terence Henriod <__t_r0d__@TMBP.local>
2024-11-22 13:00:03 -0800

    [AOC2017] Day 3

--- aoc2017/src/test/scala/day03/Day03Test.scala
+++ aoc2017/src/test/scala/day03/Day03Test.scala
@@ -2,8 +2,45 @@ package day03
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.*
+import org.scalatest.prop.TableDrivenPropertyChecks.*
 
 import day03.Day03Solution
 
-class Day03SolutionTests extends AnyFunSuite with Matchers
+class Day03SolutionTests extends AnyFunSuite with Matchers:
+    test("partOne finds the Manhattan distance to the origin on the spiral") {
+        val testCases = Table(
+            ("input", "expected"),
+            ("1","0"),
+            ("12","3"),
+            ("23","2"),
+            ("1024","31"),
+        )
+
+        forAll(testCases) { (input: String, expected: String) =>
+            val solution = Day03Solution()
+
+            val result = solution.partOne(input)
+
+            result should equal (Right(expected))
+        }
+    }
+
+    test("partTwo finds the first sum larger than the target") {
+        val testCases = Table(
+            ("input", "expected"),
+            ("0", "1"),
+            ("1", "2"),
+            ("3", "4"),
+            ("4", "5"),
+            ("58", "59"),
+        )
+
+        forAll(testCases) { (input: String, expected: String) =>
+            val solution = Day03Solution()
+
+            val result = solution.partTwo(input)
+
+            result should equal (Right(expected))
+        }
+    }
         
