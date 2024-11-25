f06e53452d32d1b0d5c1e247378a465775e1e168
Terence Henriod <__t_r0d__@TMBP.local>
2024-11-24 23:52:37 -0800

    [AOC2017] Day 05

--- aoc2017/src/main/scala/day05/Day05.scala
+++ aoc2017/src/main/scala/day05/Day05.scala
@@ -3,7 +3,68 @@ package day05
 import solution.Solution
 
 class Day05Solution extends Solution:
-    override def partOne(input: String): Either[String, String] = ???
+    override def partOne(input: String): Either[String, String] =
+        val program = parseProgram(input)
 
-    override def partTwo(input: String): Either[String, String] = ???
-        
+        val stepsToTerminate = stepsToCompleteProgram(
+            program,
+            stepsToCompleteProgramHelper
+        )
+
+        Right(stepsToTerminate.toString)
+
+    override def partTwo(input: String): Either[String, String] =
+        val program = parseProgram(input)
+
+        val stepsToTerminate = stepsToCompleteProgram(
+            program,
+            alternateStepsToCompleteProgramHelper
+        )
+
+        Right(stepsToTerminate.toString)
+
+    def parseProgram(input: String): Seq[Int] =
+        input.split("\\n").map(_.toInt).toSeq
+
+    def stepsToCompleteProgram(
+        program: Seq[Int],
+        executionPlan: (Map[Int, Int], Int, Int) => Int,
+    ): Int =
+        val mappedProgram = program.zipWithIndex.map(p => (p._2, p._1)).toMap
+        executionPlan(mappedProgram, 0, 0)
+
+    @scala.annotation.tailrec
+    final def stepsToCompleteProgramHelper(
+        program: Map[Int, Int],
+        programCounter: Int, stepsSoFar: Int
+    ): Int =
+        if !program.contains(programCounter) then
+            stepsSoFar
+        else
+            stepsToCompleteProgramHelper(
+                program.updatedWith(programCounter)(valOpt => valOpt.map(x => x + 1)), 
+                programCounter + program.getOrElse(programCounter, 0),
+                stepsSoFar + 1,
+            )
+
+    @scala.annotation.tailrec
+    final def alternateStepsToCompleteProgramHelper(
+        program: Map[Int, Int],
+        programCounter: Int, stepsSoFar: Int
+    ): Int =
+        if !program.contains(programCounter) then
+            stepsSoFar
+        else
+            alternateStepsToCompleteProgramHelper(
+                program.updatedWith(programCounter) { valOpt =>
+                    valOpt.map { x =>
+                        if x >= 3 then
+                            x - 1
+                        else
+                            x + 1
+                    }
+                }, 
+                programCounter + program.getOrElse(programCounter, 0),
+                stepsSoFar + 1,
+            )
+
