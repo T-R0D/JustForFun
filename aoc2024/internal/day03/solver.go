81f0ec095d4a871236581f2bf5a0f8523b47998e
T-R0D <thenriod@gmail.com>
2024-12-03 15:27:54 -0800

    [AOC2024] Day 03

--- aoc2024/internal/day03/solver.go
+++ aoc2024/internal/day03/solver.go
@@ -2,14 +2,146 @@ package day03
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 type Solver struct{}
 
-func (this *Solver) SolvePartOne(input string) (string, error) {
-	return "", fmt.Errorf("part 1 not implemented")
+func (s *Solver) SolvePartOne(input string) (string, error) {
+	corruptProgram := parseCorruptedProgram(input)
+
+	intactInstructions, err := parseIntactMulInstructions(corruptProgram)
+	if err != nil {
+		return "", err
+	}
+
+	sum := executeAndSumInstructions(intactInstructions)
+
+	return strconv.Itoa(sum), nil
 }
 
-func (this *Solver) SolvePartTwo(input string) (string, error) {
-	return "", fmt.Errorf("part 2 not implemented")
+func (s *Solver) SolvePartTwo(input string) (string, error) {
+	corruptProgram := parseCorruptedProgram(input)
+
+	intactInstructions, err := parseIntactMulInstructionsWithConditionals(corruptProgram)
+	if err != nil {
+		return "", err
+	}
+
+	sum := executeAndSumInstructions(intactInstructions)
+
+	return strconv.Itoa(sum), nil
+}
+
+func parseCorruptedProgram(input string) string {
+	return input
+}
+
+func parseIntactMulInstructions(program string) ([]mul, error) {
+	mulInstructionRegex, err := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	if err != nil {
+		return []mul{}, err
+	}
+
+	matches := mulInstructionRegex.FindAllStringSubmatch(program, -1)
+	if matches == nil {
+		return []mul{}, nil
+	}
+
+	intactInstructions := make([]mul, 0, len(matches))
+	for i, match := range matches {
+		a, err := strconv.Atoi(match[1])
+		if err != nil {
+			return []mul{},
+				fmt.Errorf(
+					"uncorrupted mul instruction %d ('%s'), operand A could not be parsed: %w",
+					i,
+					match[0],
+					err)
+		}
+
+		b, err := strconv.Atoi(match[2])
+		if err != nil {
+			return []mul{},
+				fmt.Errorf(
+					"uncorrupted mul instruction %d ('%s'), operand B could not be parsed: %w",
+					i,
+					match[0],
+					err)
+		}
+
+		intactInstructions = append(intactInstructions, mul{enabled: true, a: a, b: b})
+	}
+
+	return intactInstructions, nil
+}
+
+func parseIntactMulInstructionsWithConditionals(program string) ([]mul, error) {
+	mulInstructionRegex, err := regexp.Compile(`do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)`)
+	if err != nil {
+		return []mul{}, err
+	}
+
+	matches := mulInstructionRegex.FindAllStringSubmatch(program, -1)
+	if matches == nil {
+		return []mul{}, nil
+	}
+
+	intactInstructions := make([]mul, 0, len(matches))
+	enabled := true
+	for i, match := range matches {
+		if match[0] == "do()" {
+			enabled = true
+		} else if match[0] == "don't()" {
+			enabled = false
+		} else {
+			a, err := strconv.Atoi(match[1])
+			if err != nil {
+				return []mul{},
+					fmt.Errorf(
+						"uncorrupted mul instruction %d ('%s'), operand A could not be parsed: %w",
+						i,
+						match[0],
+						err)
+			}
+
+			b, err := strconv.Atoi(match[2])
+			if err != nil {
+				return []mul{},
+					fmt.Errorf(
+						"uncorrupted mul instruction %d ('%s'), operand B could not be parsed: %w",
+						i,
+						match[0],
+						err)
+			}
+
+			intactInstructions = append(intactInstructions, mul{enabled: enabled, a: a, b: b})
+		}
+	}
+
+	return intactInstructions, nil
+}
+
+type mul struct {
+	enabled bool
+	a       int
+	b       int
+}
+
+func (m *mul) Execute() int {
+	if !m.enabled {
+		return 0
+	}
+
+	return m.a * m.b
+}
+
+func executeAndSumInstructions(instructions []mul) int {
+	sum := 0
+	for _, instruction := range instructions {
+		sum += instruction.Execute()
+	}
+
+	return sum
 }
--- aoc2024/internal/day03/solver_test.go
+++ aoc2024/internal/day03/solver_test.go
@@ -9,7 +9,13 @@ func TestPartOne(t *testing.T) {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "PartOne sums result of uncorrupted `mul` instructions",
+			input: "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+			expected: "161",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -34,7 +40,13 @@ func TestPartTwo(t *testing.T) {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "PartTwo sums result of uncorrupted `mul` instructions while considering conditional instructions",
+			input: "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+			expected: "48",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
