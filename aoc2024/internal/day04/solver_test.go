a07458342e604333ba1b56face8ee9db564cfe50
T-R0D <thenriod@gmail.com>
2024-12-03 22:24:50 -0800

    [AOC2024] Day 04

--- aoc2024/internal/day04/solver_test.go
+++ aoc2024/internal/day04/solver_test.go
@@ -4,12 +4,29 @@ import (
 	"testing"
 )
 
+const exampleWordSearch = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartOne counts all instances of 'XMAS' in the word search",
+			input:    exampleWordSearch,
+			expected: "18",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -30,11 +47,17 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartTwo counts all instances of 'X-MAS'es in the word search",
+			input:    exampleWordSearch,
+			expected: "9",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
