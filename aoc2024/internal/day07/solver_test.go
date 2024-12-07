8c702cbe7d6b0a4e14da502ac4945ee0428e1696
T-R0D <thenriod@gmail.com>
2024-12-06 23:08:45 -0800

    [AOC2024] Day 07

--- aoc2024/internal/day07/solver_test.go
+++ aoc2024/internal/day07/solver_test.go
@@ -4,12 +4,28 @@ import (
 	"testing"
 )
 
+const exampleCalibrationEquations = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`
+
 func TestPartOne(t *testing.T) {
 	testCases := []struct{
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "PartOne sums the test values of the possibly true equations",
+			input: exampleCalibrationEquations,
+			expected: "3749",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -34,7 +50,13 @@ func TestPartTwo(t *testing.T) {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "PartTwo sums the test values of the possibly true equations using the concat operator",
+			input: exampleCalibrationEquations,
+			expected: "11387",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
