f4380929b79ed0d24ae7911303fb87c85a9800cc
T-R0D <thenriod@gmail.com>
2024-12-07 21:10:54 -0800

    [AOC2017] Day 17

--- aoc2017/src/test/scala/day17/Day17Test.scala
+++ aoc2017/src/test/scala/day17/Day17Test.scala
@@ -5,5 +5,15 @@ import org.scalatest.matchers.should.*
 
 import day17.Day17Solution
 
-class Day17SolutionTests extends AnyFunSuite with Matchers
+class Day17SolutionTests extends AnyFunSuite with Matchers {
+    test("partOne predicts the next position of the spinlock after 2017 iterations") {
+        val input = "3"
+        val expected = "638"
+        val solution = Day17Solution()
+
+        val result = solution.partOne(input)
+
+        result should equal(Right(expected))
+    }
+}
         
