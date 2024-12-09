8100e4d35c340466affaa49801f427f08fceef05
T-R0D <thenriod@gmail.com>
2024-12-08 23:23:16 -0800

    [AOC2024] Day 09

--- aoc2024/internal/day09/solver_test.go
+++ aoc2024/internal/day09/solver_test.go
@@ -5,11 +5,17 @@ import (
 )
 
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartOne computes the checksum of the compacted disk",
+			input:    "2333133121414131402",
+			expected: "1928",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -30,11 +36,17 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartTwo computes the checksum of the compacted disk (without fragmenting files)",
+			input:    "2333133121414131402",
+			expected: "2858",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
