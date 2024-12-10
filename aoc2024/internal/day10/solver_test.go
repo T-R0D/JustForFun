bcd127cc6e360f755ec1dd0cfae3acfa16362df8
T-R0D <thenriod@gmail.com>
2024-12-09 22:46:46 -0800

    [AOC2024] Day 10

--- aoc2024/internal/day10/solver_test.go
+++ aoc2024/internal/day10/solver_test.go
@@ -4,12 +4,27 @@ import (
 	"testing"
 )
 
+const sampleTopographicMap = `89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732`
+
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartOne sums the scores for all trailheads",
+			input:    sampleTopographicMap,
+			expected: "36",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -30,11 +45,17 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartTwo sums the ratings for all trailheads",
+			input:    sampleTopographicMap,
+			expected: "81",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
