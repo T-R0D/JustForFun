f6e7a051882e722855038431bd56b0884c594fd7
T-R0D <thenriod@gmail.com>
2024-12-10 16:21:06 -0800

    [AOC2017] Day 19

--- aoc2017/src/test/scala/day19/Day19Test.scala
+++ aoc2017/src/test/scala/day19/Day19Test.scala
@@ -5,5 +5,37 @@ import org.scalatest.matchers.should.*
 
 import day19.Day19Solution
 
-class Day19SolutionTests extends AnyFunSuite with Matchers
+class Day19SolutionTests extends AnyFunSuite with Matchers {
+    test("partOne collects the letters on the path in the correct order") {
+        val input = """#    |          
+                       #    |  +--+    
+                       #    A  |  C     
+                       #F---|----E|--+
+                       #    |  |  |  D 
+                       #    +B-+  +--+ 
+                       #""".stripMargin('#')
+        val expected = "ABCDEF"
+        val solution = Day19Solution()
+
+        val result = solution.partOne(input)
+
+        result should equal(Right(expected))
+    }
+
+    test("partOne counts the steps to walk the path") {
+        val input = """#    |          
+                       #    |  +--+    
+                       #    A  |  C     
+                       #F---|----E|--+
+                       #    |  |  |  D 
+                       #    +B-+  +--+ 
+                       #""".stripMargin('#')
+        val expected = "38"
+        val solution = Day19Solution()
+
+        val result = solution.partTwo(input)
+
+        result should equal(Right(expected))
+    }
+}
         
