06f1c155885f3c0e1f2118404b983547973c0e29
T-R0D <thenriod@gmail.com>
2024-12-13 17:39:34 -0800

    [AOC2024] Day 22

--- aoc2017/src/test/scala/day22/Day22Test.scala
+++ aoc2017/src/test/scala/day22/Day22Test.scala
@@ -5,5 +5,29 @@ import org.scalatest.matchers.should.*
 
 import day22.Day22Solution
 
-class Day22SolutionTests extends AnyFunSuite with Matchers
+class Day22SolutionTests extends AnyFunSuite with Matchers {
+    test("partOne counts the times the infection is distributed") {
+        val input = """|..#
+                       |#..
+                       |...""".stripMargin
+        val expected = "5587"
+        val solution = Day22Solution()
+
+        val result = solution.partOne(input)
+
+        result should equal(Right(expected))
+    }
+    
+    test("partTwo counts the times the infection is distributed by the evolved virus") {
+        val input = """|..#
+                       |#..
+                       |...""".stripMargin
+        val expected = "2511944"
+        val solution = Day22Solution()
+
+        val result = solution.partTwo(input)
+
+        result should equal(Right(expected))
+    }
+}
         
