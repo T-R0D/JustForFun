cece55764d79c1e1c93dd34c9c511585cf740328
T-R0D <thenriod@gmail.com>
2024-12-13 23:48:35 -0800

    Complete part 2

--- aoc2017/src/main/scala/day23/Day23.scala
+++ aoc2017/src/main/scala/day23/Day23.scala
@@ -84,13 +84,48 @@ class Day23Solution extends Solution {
     }
     
     def runSimplifiedProgramInReleaseMode(instructions: Seq[Instruction]): Int = {
-        // In the input (mine at least), the first 8 instructions  form the prologue.
+        // This was a "read the assembly and translate" exercise.
+        // The program basically breaks down into 2 phases: an initialization
+        // prologue and a triple nested loop that sometimes increments the 'h'
+        // register.
+        // The prologue is where I assume the input uniqueness is, in the form
+        // of some constants that vary from participant to participant. I
+        // would guess that those constants are the values that are put into
+        // registers 'b' and 'c'. Setting the "release mode" flag (a = 1) will
+        // dramatically increase their values.
+        // The main body of the program essentially is 3 nested loops.
+        // The first/outermost is a loop that counts up from 'b' to 'c',
+        // incrementing 'b' by 17 (I assume this is not participant specific?).
+        // This loop will sometimes increment 'h' if a flag has been set. The
+        // flag is reset each iteration and will be set within the two further
+        // nested loops.
+        // The next loop essentially counts up from 2 to the current value of
+        // 'b'. It uses 'd' as a counter variable.
+        // The third and most nested loop also counts from 2 to the current
+        // value of 'b', using 'e' as a counter variable. However, inside this 
+        // loop, there is a test to see if 'd' multiplied with 'e' equal the
+        // current value of 'b', and if so, the aforementioned flag is set.
+        // Throughout these loops, 'g' is used as a sort of temp variable, but
+        // it's kind of a red herring.
+        // In short, the program can be simply described as "Loop from a
+        // starting value to an ending one by some increment, and for each 
+        // intermediate value, check if there is a factorization of the
+        // intermediate value (other than 1 and itself)".
+        // I'm not sure if it was necessary, but it provided a lot of speed up,
+        // when checking for factors, I only used prime numbers as candidates
+        // since I figured all non-prime numbers have prime factors. This
+        // list was much shorter than trying every number from 2 to the
+        // intermediate value. Each time a number had a prime factor, then
+        // increment 'h' once for that number. (I probably could have used
+        // a nice for loop and tried even non-primes, but this is a functional
+        // language, so whiles and breaks and such are a pain to implement.)
+
+        // In the input (mine at least), the first 8 instructions  form the
+        // prologue.
         val prologueInstructions = instructions.take(8)
         val mainProgramInstructions = instructions.drop(8)
 
         val initializedRegisters = runProgramPrologueInReleaseMode(prologueInstructions)
-
-        println(s"$initializedRegisters")
 
         val cRegisterValue = initializedRegisters.getOrElse('c', 0)
         val primes = generatePrimesUpTo(cRegisterValue)
@@ -156,13 +191,14 @@ class Day23Solution extends Solution {
 
         val finalHRegisterValue = (initialBRegisterValue to cRegisterValue by 17)
             .foldLeft(0) { (acc, b) =>
+                val primesLessThanB = primes.filter(_ < b)
                 val factorizationExists = !(
                     for {
-                        d <- 2 to b
-                        e <- 2 to b
-                        if d * e == b
+                        prime <- primesLessThanB
+                        quotient = b / prime
+                        mod = b % prime
+                        if mod == 0 && quotient >= 2
                     } yield {
-                        println(s"$d * $e = $b")
                         true
                     }
                 ).isEmpty
