c9b7cc3ef732785bd65658f82b757878b82fad50
T-R0D <thenriod@gmail.com>
2024-12-13 23:22:29 -0800

    [AOC2024] Day 14

--- aoc2024/internal/day14/solver_test.go
+++ aoc2024/internal/day14/solver_test.go
@@ -4,20 +4,37 @@ import (
 	"testing"
 )
 
+const exampleRobotInitials = `p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3`
+
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartOne computes the safety factor after 100 seconds of robot movement",
+			input:    exampleRobotInitials,
+			expected: "12",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			solver := Solver{}
-
-			result, err := solver.SolvePartOne(tc.input)
+			result, err := configurablePartOne(tc.input, [2]int{7, 11})
 			if err != nil {
 				t.Error("unable to complete solution", err)
 			}
@@ -30,7 +47,7 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
