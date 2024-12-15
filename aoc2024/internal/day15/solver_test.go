487626a2f5ce5742e6e8ff1ca7464b1a780e9bb8
T-R0D <thenriod@gmail.com>
2024-12-15 01:28:22 -0800

    [AOC2024] Day 15

--- aoc2024/internal/day15/solver_test.go
+++ aoc2024/internal/day15/solver_test.go
@@ -4,12 +4,66 @@ import (
 	"testing"
 )
 
+const smallExampleWarehouseAndRobotMovements = `########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<`
+
+const secondSmallExampleWarehouseAndRobotMovements = `#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^`
+
+const exampleWarehouseAndRobotMovements = `##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^`
+
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartOne finds the sum of the box Goods Positioning Sysytem coordinates for a small example",
+			input:    smallExampleWarehouseAndRobotMovements,
+			expected: "2028",
+		},
+		{
+			name:     "PartOne finds the sum of the box Goods Positioning Sysytem coordinates",
+			input:    exampleWarehouseAndRobotMovements,
+			expected: "10092",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -30,11 +84,22 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "PartTwo finds the sum of the box Goods Positioning System coordinates for a scaled up warehouse in a second small example",
+			input: secondSmallExampleWarehouseAndRobotMovements,
+			expected: "618",
+		},
+		{
+			name: "PartTwo finds the sum of the box Goods Positioning System coordinates for a scaled up warehouse",
+			input: exampleWarehouseAndRobotMovements,
+			expected: "9021",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
