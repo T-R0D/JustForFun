521243aa2ae82b1a4de148a1fd5bed9022a01776
T-R0D <thenriod@gmail.com>
2024-12-18 22:52:17 -0800

    [AOC2024] Day 19

--- aoc2024/internal/day19/solver_test.go
+++ aoc2024/internal/day19/solver_test.go
@@ -4,12 +4,29 @@ import (
 	"testing"
 )
 
+const exampleTowelsAndPatterns = `r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`
+
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartOne counts the achievable patterns with the given towels",
+			input:    exampleTowelsAndPatterns,
+			expected: "6",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -30,11 +47,17 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:     "PartTwo counts all the ways to achieve the patterns",
+			input:    exampleTowelsAndPatterns,
+			expected: "16",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
