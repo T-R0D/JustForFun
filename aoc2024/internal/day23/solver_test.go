9ffc14089cf0e30e4c694a6a98df31dd55659288
T-R0D <thenriod@gmail.com>
2024-12-23 16:26:07 -0800

    [AOC2024] Day 23

--- aoc2024/internal/day23/solver_test.go
+++ aoc2024/internal/day23/solver_test.go
@@ -4,12 +4,51 @@ import (
 	"testing"
 )
 
+const exampleConnectivityList = `kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn`
+
 func TestPartOne(t *testing.T) {
 	testCases := []struct{
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "PartOne counts cliques of 3 with at least one 't.*' member",
+			input: exampleConnectivityList,
+			expected: "7",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -34,7 +73,13 @@ func TestPartTwo(t *testing.T) {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name:"PartTwo finds the name of every computer in the largest clique, sorted and joined with commas",
+			input: exampleConnectivityList,
+			expected: "co,de,ka,ta",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
