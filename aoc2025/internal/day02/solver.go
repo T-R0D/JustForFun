54b41e117789bba50cf786c9262744044b1f3130
T-R0D <thenriod@gmail.com>
2025-12-02 09:22:12 -0800

    [AOC2025] Day 02

--- aoc2025/internal/day02/solver.go
+++ aoc2025/internal/day02/solver.go
@@ -1,15 +1,127 @@
 package day02
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
 )
 
 type Solver struct{}
 
 func (this *Solver) SolvePartOne(input string) (string, error) {
-	return "", fmt.Errorf("part 1 not implemented")
+	ranges, err := parseIDRanges(input)
+	if err != nil {
+		return "", err
+	}
+
+	invalidIdSum := 0
+	for _, r := range ranges {
+		for x := r.first; x <= r.last; x += 1 {
+			if isRepeatedNumber(x) {
+				invalidIdSum += x
+			}
+		}
+	}
+
+	return strconv.Itoa(invalidIdSum), nil
 }
 
 func (this *Solver) SolvePartTwo(input string) (string, error) {
-	return "", fmt.Errorf("part 2 not implemented")
+	ranges, err := parseIDRanges(input)
+	if err != nil {
+		return "", err
+	}
+
+	invalidIdSum := 0
+	for _, r := range ranges {
+		for x := r.first; x <= r.last; x += 1 {
+			if isSequenceOfRepeats(x) {
+				invalidIdSum += x
+			}
+		}
+	}
+
+	return strconv.Itoa(invalidIdSum), nil
+}
+
+type idRange struct {
+	first int
+	last  int
+}
+
+func parseIDRanges(input string) ([]idRange, error) {
+	ranges := strings.Split(input, ",")
+	parsedRanges := make([]idRange, 0, len(ranges))
+
+	for _, rangeStr := range ranges {
+		parts := strings.Split(rangeStr, "-")
+		first, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return []idRange{}, err
+		}
+		last, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return []idRange{}, err
+		}
+
+		parsedRanges = append(parsedRanges, idRange{first, last})
+	}
+
+	return parsedRanges, nil
+}
+
+func isRepeatedNumber(x int) bool {
+	digits := intToDigitArray(x)
+	nDigits := len(digits)
+
+	if nDigits&1 == 1 {
+		return false
+	}
+
+	halfLen := nDigits / 2
+	for i := range halfLen {
+		if digits[i] != digits[i+halfLen] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isSequenceOfRepeats(x int) bool {
+	digits := intToDigitArray(x)
+	nDigits := len(digits)
+	halfLen := nDigits / 2
+
+NextSequenceCandidate:
+	for sequenceLen := 1; sequenceLen <= halfLen; sequenceLen += 1 {
+		if nDigits%sequenceLen != 0 {
+			continue NextSequenceCandidate
+		}
+
+		for nextSequenceStart := sequenceLen; nextSequenceStart < nDigits; nextSequenceStart += sequenceLen {
+			for i := range sequenceLen {
+				if digits[i] != digits[nextSequenceStart + i] {
+					continue NextSequenceCandidate
+				}
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
+
+func intToDigitArray(x int) []int {
+	if x == 0 {
+		return []int{0}
+	}
+
+	digits := []int{}
+	for x != 0 {
+		digits = append(digits, x%10)
+		x /= 10
+	}
+
+	return digits
 }
--- aoc2025/internal/day02/solver_test.go
+++ aoc2025/internal/day02/solver_test.go
@@ -5,11 +5,17 @@ import (
 )
 
 func TestPartOne(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "Part one sums invalid IDs (twice repeated sequence) to 1227775554",
+			input: testInput,
+			expected: "1227775554",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -30,11 +36,17 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
 		name     string
 		input    string
 		expected string
-	}{}
+	}{
+		{
+			name: "Part two sums invalid IDs (repeated sequence twice or more) to 4174379265",
+			input: testInput,
+			expected: "4174379265",
+		},
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
@@ -53,3 +65,5 @@ func TestPartTwo(t *testing.T) {
 		})
 	}
 }
+
+const testInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
